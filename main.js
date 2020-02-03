7a6ca74437852d554e6a8af27d3b58ba7757b7df
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-03 15:16:21 -0600

    finished lightning exercises

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import businesses from './businesses.js';
 
-const outEl = document.querySelector("#output")
+// const outEl = document.querySelector("#output")
 // outEl.innerHTML = "<h1>Active Businesses</h1>"
 
 // Array to contain all businesses
@@ -60,28 +60,99 @@ const outEl = document.querySelector("#output")
 
 // Array to contain all the Purchasing Agents
 
-outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+// outEl.innerHTML += "<h1>Purchasing Agents</h1>";
 
 /*
     Using map(), you extract the purchasing agent object
     from each business and store it in a new array
 */
-const agents = businesses.map(business => {
-    return {
-        "firstName": business.purchasingAgent.nameFirst,
-        "lastName": business.purchasingAgent.nameLast,
-        "company": business.companyName,
-        "phoneNumber": business.phoneWork
-    }
-})
+// const agents = businesses.map(business => {
+//     return {
+//         "firstName": business.purchasingAgent.nameFirst,
+//         "lastName": business.purchasingAgent.nameLast,
+//         "company": business.companyName,
+//         "phoneNumber": business.phoneWork
+//     }
+// })
 
-console.table(agents)
+// console.table(agents)
 
-agents.forEach(agent => {
-  outEl.innerHTML += 
-  `<h2>${agent.firstName} ${agent.lastName}</h2>
-  <p>${agent.company}</p>
-  <p>${agent.phoneNumber}</p>`;
-  outEl.innerHTML += "<hr/>";
-});
+// agents.forEach(agent => {
+//   outEl.innerHTML += 
+//   `<h2>${agent.firstName} ${agent.lastName}</h2>
+//   <p>${agent.company}</p>
+//   <p>${agent.phoneNumber}</p>`;
+//   outEl.innerHTML += "<hr/>";
+// });
 
+
+// Find a specific business
+
+// document
+//     .querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundBusiness = businesses.find(
+//                 business =>
+//                     business.companyName.includes(keyPressEvent.target.value)
+//             );
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundBusiness.companyName}
+//                 </h2>
+//                 <section>
+//                 ${foundBusiness.addressFullStreet}
+
+//                 </section>
+//                 <section>
+//                 ${foundBusiness.addressCity},
+//                 ${foundBusiness.addressStateCode}
+//                 ${foundBusiness.addressZipCode}
+//                 </section>
+//             `;
+//         }
+//     });
+
+// document
+//     .querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundBusiness = businesses.find(
+//                 business =>
+//                     business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value) || business.purchasingAgent.nameLast.includes(keyPressEvent.target.value)
+//             );
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundBusiness.purchasingAgent.nameFirst}                 ${foundBusiness.purchasingAgent.nameLast} 
+
+//                 </h2>
+//             `;
+//         }
+//     });
+
+
+// Calculate order total
+
+/* CALCULATE ORDER SUMMARY */
+// let totalOrders = business.orders.reduce(
+//     (currentTotal, nextValue) => currentTotal += nextValue,
+//     0
+// )
+
+
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+const totalRainfall = monthlyRainfall.reduce((currentTotal, currentValue) => currentTotal + currentValue);
+
+console.log(totalRainfall)
+
+
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+const sentence = words.reduce((sentence, currentWord) => sentence + ' ' + currentWord)
+
+console.log(sentence)
