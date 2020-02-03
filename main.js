1ca7baa1f0cbbc1befd988f52fe3b4083308f37b
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-03 13:39:46 -0600

    finished second lightning exercise

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
 import businesses from './businesses.js';
 
 const outEl = document.querySelector("#output")
-outEl.innerHTML = "<h1>Active Businesses</h1>"
+// outEl.innerHTML = "<h1>Active Businesses</h1>"
 
-businesses.forEach(business => {
-  outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-    <section>
-    ${business.addressCity}, ${business.addressStateCode} ${business.addressZipCode}
-    </section>
-  `
-  outEl.innerHTML += "<hr/>"
-});
+// Array to contain all businesses
+
+// const allBusinesses = businesses.forEach(business => {
+//   outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//     ${business.addressCity}, ${business.addressStateCode} ${business.addressZipCode}
+//     </section>
+//   `
+//   outEl.innerHTML += "<hr/>"
+// });
 
 
+// Array to contain all the New York businesses
+// const newYorkBusinesses = businesses.filter(business => {
+//     let inNewYork = false
+  
+//     if (business.addressStateCode === "NY") {
+//         inNewYork = true
+//     }
+  
+//     return inNewYork
+//   })
+
+  // Array to contain all the Manufacturing businesses
+      
+  outEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
+
+  const manufacturingBusinesses = businesses.filter(business => {
+      let isManufacturing = false
+
+      if (business.companyIndustry === "Manufacturing") {
+          isManufacturing = true
+      }
+      return isManufacturing
+  })
+
+//   console.log(manufacturingBusinesses);
+
+manufacturingBusinesses.forEach(business => {
+    outEl.innerHTML += `
+      <h2>${business.companyName}</h2>
+      <section>
+        ${business.addressFullStreet}
+      </section>
+      <section>
+      ${business.addressCity}, ${business.addressStateCode} ${business.addressZipCode}
+      </section>
+    `
+    outEl.innerHTML += "<hr/>"
+  });
